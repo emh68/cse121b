8300801a5db455b4f21862b874ca14d19182dc9a
emh68 <elihansen@yahoo.com>
2024-01-19 20:19:27 -0700

    week02 edits

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -5,26 +5,29 @@
 /* Step 2 - Variables */
 let fullName = 'Eli Hansen';
 let currentYear = 2024;
-let profilePicture = 'images/profilpicoriginal.jpg';
-
+let profilePicture = 'images/profilepicoriginal.jpg';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+let yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
+let favoriteFoods = ['Fettucine Alfredo', 'Pizza', 'Fajitas', 'Ice Cream', 'Caramel Pie'];
+desert = 'Rice Crispy Treats';
+favoriteFoods.push(desert);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
