27a49ccd426adef0bd3818da594aa35304467802
emh68 <elihansen@yahoo.com>
2024-01-24 17:53:31 -0700

    edits

--- w03-task/scripts/w03-task.js
+++ w03-task/scripts/w03-task.js
@@ -49,18 +49,50 @@ document.querySelector('#divideNumbers').addEventListener('click', divideNumbers
 
 /* Decision Structure */
 
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#getTotal').addEventListener('click', function () {
+        // Get the value entered in the subtotal field and store it as the variable 'subtotal'
+        let subtotal = Number(document.querySelector('#subtotal').value);
 
+        // Check if the input is a valid numeric amount
+        if (isNaN(subtotal)) {
+            alert('Please enter a valid numeric amount for the subtotal.');
+        } else {
+            // Check if the member checkbox is checked
+            let isMember = document.getElementById('member').checked;
+
+            // Apply a 20% discount if the user is a member
+            if (isMember) {
+                subtotal *= 0.8; // 20% discount
+            }
+
+            // Set the 'total' value based on the conditional function
+            let total = subtotal;
+
+            // Output the total to the total span in the format shown with two decimals
+            document.getElementById('total').textContent = `$ ${total.toFixed(2)}`;
+        }
+    });
+});
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
-
+let numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.getElementById('array').textContent = numsArray;
 /* Output Odds Only Array */
-
+let oddNums = numsArray.filter(num => num % 2 !== 0);
+document.getElementById('odds').textContent = oddNums;
 /* Output Evens Only Array */
-
+let evenNums = numsArray.filter(num => num % 2 === 0);
+document.getElementById('evens').textContent = evenNums;
 /* Output Sum of Org. Array */
-
+let sumOfArray = numsArray.reduce((sum, number) => sum + number);
+document.getElementById('sumOfArray').textContent = sumOfArray
 /* Output Multiplied by 2 Array */
-
+let multipliedArray = numsArray.map(number => number * 2);
+document.getElementById('multiplied').textContent = multipliedArray;
 /* Output Sum of Multiplied by 2 Array */
+let multiplyArray = numsArray.map(number => number * 2);
+let sumOfMultiplied = multiplyArray.reduce((sum, number) => sum + number);
+document.getElementById('sumOfMultiplied').textContent = sumOfMultiplied;
