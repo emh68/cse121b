5cef8bd32512da51802611adee2d7c95507b8fdb
emh68 <elihansen@yahoo.com>
2024-01-29 17:33:34 -0700

    moved files out of separate folders for each week

---
+++ scripts/w04-task.js
@@ -0,0 +1,82 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Eli Hansen",
+    photo: "images/profilepicoriginal.jpg",
+    favoriteFoods: ['Fettucine Alfredo', 'Pizza', 'Fajitas', 'Ice Cream', 'Caramel Pie'],
+    hobbies: ['Travel', 'Tennis', 'Baseball', 'Water Sports', 'Hiking', 'Guitar', 'Singing', 'Music', 'Electronics'],
+    placesLived: []
+
+};
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: 'Kuna, ID',
+        length: '1 year',
+    },
+    {
+        place: 'West Lafayette, IN',
+        length: '6 years',
+    },
+    {
+        place: 'Rexburg, ID',
+        length: '15 years',
+    },
+    {
+        place: 'Denver, CO',
+        length: '1 year',
+    },
+    {
+        place: 'Colorado, Springs, CO',
+        length: '1 year',
+    },
+    {
+        place: 'Bastogne, BE',
+        length: '4 months',
+    },
+    {
+        place: 'Gennevilliers-Asnieres, FR',
+        length: '8 months',
+    },
+    {
+        place: 'Provo, UT',
+        length: '7 years',
+    },
+    {
+        place: 'Ketchikan, AK',
+        length: '4 months',
+    },
+    {
+        place: 'Draper, UT',
+        length: '3 years',
+    },
+    {
+        place: 'Bluffdale, UT',
+        length: '5 years',
+    },
+    {
+        place: 'Lehi, UT',
+        length: '1 year',
+    }
+);
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
